b918d098605420f35057065d00d4f816100fa74c
Carbon2301 <trinhhuuan372@gmail.com>
2024-11-12 01:07:40 +0700

    Sửa RMQ

--- Applied Algorithms/Practice/Lab01/Range_minimum_query.cpp
+++ Applied Algorithms/Practice/Lab01/Range_minimum_query.cpp
@@ -1,54 +1,64 @@
-#include <iostream>     
-#include <vector>       
-#include <cmath>        // Cho hàm log2
-#include <algorithm>    // Cho hàm min
+#include <iostream>
+#include <vector>
+#include <cmath>
 
 using namespace std;
 
-const int MAXN = 100001; 
-const int LOG = 20; 
+const int MAXN = 1000001;
+const int LOGN = 20;  
 
-int arr[MAXN];
-int st[MAXN][LOG]; 
+int st[MAXN][LOGN];
+int logs[MAXN];
 
-void build(int n) {
+void preprocess(const vector<int>& arr) {
+    int n = arr.size();
+    
+    logs[1] = 0;
+    for (int i = 2; i <= n; i++) {
+        logs[i] = logs[i/2] + 1;
+    }
+    
     for (int i = 0; i < n; i++) {
         st[i][0] = arr[i];
     }
     
     for (int j = 1; (1 << j) <= n; j++) {
-        for (int i = 0; i + (1 << j) - 1 < n; i++) {
-            st[i][j] = min(st[i][j - 1], st[i + (1 << (j - 1))][j - 1]);
+        for (int i = 0; i + (1 << j) <= n; i++) {
+            st[i][j] = min(st[i][j-1], st[i + (1 << (j-1))][j-1]);
         }
     }
 }
 
 int query(int L, int R) {
-    int j = log2(R - L + 1);
+    int j = logs[R - L + 1];
     return min(st[L][j], st[R - (1 << j) + 1][j]);
 }
 
 int main() {
-    int n; 
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
     cin >> n;
-    
+
+    vector<int> arr(n);
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
-    
-    build(n);
 
-    int m; 
+    preprocess(arr);
+
+    int m;
     cin >> m;
-    int result = 0;
 
+    long long Q = 0;
     for (int k = 0; k < m; k++) {
         int i, j;
         cin >> i >> j;
-        result += query(i, j);
+        Q += query(i, j);
     }
 
-    cout << result << endl;
+    cout << Q << endl;
+
     return 0;
 }
-
